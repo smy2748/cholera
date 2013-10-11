3abb9d29ce068e2e416b4c8175112b07e0ed5197
Stephen Yingling <smy2748@rit.edu>
2013-10-11 17:14:28 -0400

    Generations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cholera::Application.config.secret_key_base = '51534c97333d70d1f686c0accd0d2e7e71c6891914c7ddecd1ba5e9b71bb8d061c1d29694d4f11cf56d6e8f8c13b326c8d490fc8fbf2626d8272e8e6842c2ed4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cholera::Application.config.session_store :cookie_store, key: '_cholera_session'
